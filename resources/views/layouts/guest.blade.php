f6a26672ce3357b320d5ea109717bca29d9ad053
shaitr1x <dokoalain17@icloud.com>
2025-08-20 18:00:02 +0100

    Fix: Ajout universel de @vite dans le layout guest pour le CSS/JS login et inscription

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,13 +12,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @if(app()->environment('local'))
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @else
-            <link rel="stylesheet" href="{{ asset('build/assets/app-49e025ce.css') }}">
-            <script src="{{ asset('build/assets/app-4ba226a4.js') }}" defer></script>
-        @endif
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
