05c1f393eeb56b5b49d614ef5d64334083ca2617
shaitr1x <dokoalain17@icloud.com>
2025-08-21 12:54:43 +0100

    Ajout balise META Bing pour validation Webmaster Tools

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
     <meta charset="utf-8">
+    <meta name="msvalidate.01" content="7B099342CE9E70FAB0B953191736C993" />
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
